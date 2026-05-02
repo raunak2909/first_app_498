0d9c5230fcc961d328a5f88a64b7cd00f67a2ea3
wscubegoodtogo-beep <wscube.goodtogo@gmail.com>
2026-05-02 20:56:32 +0530

    text, center, align widget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,11 +7,28 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hello World!"),
+      body: Align(
+        alignment: Alignment(0, 0),
+          child: Text("Hello World!rnvlwkrnvklrnvklfnvvfsbjlvkwbdnlkvnwbdlkvnlkwdnvlwdknvlkwdnvlkwdnvklwdnvlknwdlkvnwdklnvwkldnvlkwdnvlkwdn", style: TextStyle(
+            fontSize: 40,
+            //fontWeight: FontWeight.bold,
+            //fontStyle: FontStyle.italic,
+            //fontFamily: "cursive",
+            //color: Colors.amber,
+            //backgroundColor: Colors.blue,
+            /*decoration: TextDecoration.lineThrough,
+            decorationColor: Colors.black,
+            decorationStyle: TextDecorationStyle.double*/
+              //letterSpacing: 11,
+              //wordSpacing: 11,
+          ),
+            //textAlign: TextAlign.start,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 4,
+          )
       ),
       appBar: AppBar(
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
         title: Text("Home"),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
+      theme: ThemeData(
+        fontFamily: "cursive"
+      ),
     );
   }
 }

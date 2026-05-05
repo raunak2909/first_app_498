c6008c98bdfb8909b6951a0e26c58b5d34c467b5
wscubegoodtogo-beep <wscube.goodtogo@gmail.com>
2026-05-05 20:53:16 +0530

    container widget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget{
 
@@ -8,24 +9,35 @@ class HomePage extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       body: Align(
-        alignment: Alignment(0, 0),
-          child: Text("Hello World!rnvlwkrnvklrnvklfnvvfsbjlvkwbdnlkvnwbdlkvnlkwdnvlwdknvlkwdnvlkwdnvklwdnvlknwdlkvnwdklnvwkldnvlkwdnvlkwdn", style: TextStyle(
-            fontSize: 40,
-            //fontWeight: FontWeight.bold,
-            //fontStyle: FontStyle.italic,
-            //fontFamily: "cursive",
-            //color: Colors.amber,
-            //backgroundColor: Colors.blue,
-            /*decoration: TextDecoration.lineThrough,
-            decorationColor: Colors.black,
-            decorationStyle: TextDecorationStyle.double*/
-              //letterSpacing: 11,
-              //wordSpacing: 11,
+        alignment: Alignment.center,
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+              color: Color(0xff0000ff),
+              borderRadius: BorderRadius.circular(21),
+              /*border: Border.all(
+                width: 21,
+                color: Colors.black.withAlpha(100),
+                strokeAlign: BorderSide.strokeAlignInside
+              ),*/
+              //shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 11,
+                  spreadRadius: 1,
+                  offset: Offset(-11, 11)
+                )
+              ]
           ),
-            //textAlign: TextAlign.start,
-            overflow: TextOverflow.ellipsis,
-            maxLines: 4,
-          )
+          child: Align(
+            alignment: Alignment.center,
+              child: Text("Hello World!!", style: TextStyle(
+                fontSize: 25
+              ),)
+          ),
+        ),
       ),
       appBar: AppBar(
         backgroundColor: Colors.blue,
@@ -36,3 +48,28 @@ class HomePage extends StatelessWidget{
   }
 
 }
+
+///Align(
+//         alignment: Alignment(0, 0),
+//           child: Text('Hello World', style: GoogleFonts.oswald(
+//             fontSize: 34
+//           ),)
+//       ),
+
+///Text("Hello World!rnvlwkrnvklrnvklfnvvfsbjlvkwbdnlkvnwbdlkvnlkwdnvlwdknvlkwdnvlkwdnvklwdnvlknwdlkvnwdklnvwkldnvlkwdnvlkwdn", style: TextStyle(
+//             fontSize: 40,
+//             //fontWeight: FontWeight.bold,
+//             //fontStyle: FontStyle.italic,
+//             //fontFamily: "cursive",
+//             //color: Colors.amber,
+//             //backgroundColor: Colors.blue,
+//             /*decoration: TextDecoration.lineThrough,
+//             decorationColor: Colors.black,
+//             decorationStyle: TextDecorationStyle.double*/
+//               //letterSpacing: 11,
+//               //wordSpacing: 11,
+//           ),
+//             //textAlign: TextAlign.start,
+//             overflow: TextOverflow.ellipsis,
+//             maxLines: 4,
+//           )
